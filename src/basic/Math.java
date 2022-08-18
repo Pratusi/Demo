d33420afcfd28b92795848cb9d5eb9c86ca71e04
Pratusi <110887485+PratikshaBansode@users.noreply.github.com>
2022-08-18 16:03:22 +0530

    Math.java

---
+++ src/basic/Math.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class Math {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Addition");
+
+        Calculator.add(22, 67);
+
+        Calculator.add(65, 89, 43);
+
+        System.out.println("Multiplication");
+
+        Calculator.multiply(23, 8);
+		
+
+	}
+
+}
